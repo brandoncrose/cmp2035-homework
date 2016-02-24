7f0307b9ed5289591df52035ad5ae379f02d0135
astrid23 <WENBIN.WENG@TUFTS.EDU>
2016-02-24 02:39:37 -0500

    CSS
    and video demo

---
+++ 04/pythonworkshop.py
@@ -0,0 +1,39 @@
+A = 100
+B = 300
+C = STRING
+print type class (object):
+	"""docstring for """
+	def __init__(self, arg):
+		super(, self).__init__()
+		self.arg = arg
+print C + ""
+print str(A)
+# data structures
+# list
+listofwww = [" different type of variables"]
+print listofwww[1]
+print listofwww [0:2]
+print len(listofwww)
+for album in listofwww
+print "" + albumn
+albumscores ={
+	"a" :0
+
+}
+print albumscores["]"]
+	def keyword(nameofbbb):
+	score = []
+	for album in albumnames
+	albumscore 
+	socres.append()	
+	return socres
+print albumscore([albumnames])
+
+
+#datacamp intro to python of data science
+numpy
+import numpy as numpy
+import matplotlib
+
+x = np.linspace(0, 2*np, 100)
+plt.
